668b8bd8edc0619a87bfdc69a29dff05bd6e1f2f
MbappeWang <wys9404@gmail.com>
2024-12-07 21:39:59 +0800

    优化赛事数据导出功能；更新导出路径并修复相关错误

---
+++ 邮件发送程序.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import filedialog
+import smtplib
+import ssl
+from email.header import Header
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formataddr
+
