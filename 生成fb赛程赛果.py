3e2cd11a2b31dcb3f928ff01dc38e3fc1a9e5702
MbappeWang <wys9404@gmail.com>
2024-12-10 02:03:39 +0800

    新增 SQLAlchemy 相关文件和类型注解；更新安装器和轮子文件以支持新版本

--- 生成fb赛程赛果.py
+++ 生成fb赛程赛果.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import requests
+import pytz
+from datetime import datetime
+from sqlalchemy import create_engine
+import mysql.connector
 
 def getResults(beginTime, endTime, languageType, sportId):
     url = f'https://sportapi.fastball2.com/v1/match/matchResultList'
@@ -37,7 +41,67 @@ def resultParse(matchDict):
     matchInfo['awayTeamImage'] = matchDict['ts'][1].get('lurl', None)
     return matchInfo
 
-responesResult = getResults(1733673600000, 1733759999999, "ENG", 1)
-data = responesResult.get('data', None)
-if data is not None:
-    matches = data.get('data', None)
+def one_day_result(beginTime, endTime):
+    responesResult = getResults(beginTime, endTime, "ENG", 1)
+    data = responesResult.get('data', None)
+    matchesinfo = []
+    if data is not None:
+        for match in data:
+            matchInfo = resultParse(match)
+            matchesinfo.append(matchInfo)
+    return matchesinfo
+
+def getUnixTime(date):
+    local_timezone = pytz.timezone('Asia/Shanghai')
+    local_time = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+    local_time_with_tz = local_timezone.localize(local_time)
+    utc_time = local_time_with_tz.astimezone(pytz.utc)
+    unix_timestamp_with_ms = int(utc_time.timestamp() * 1000)
+    return unix_timestamp_with_ms
+
+startDate = '2023-12-10 00:00:00'
+searchDate = 365
+beginTime = getUnixTime(startDate)
+
+# MySQL 数据库配置
+db_config = {
+    'user': 'sportData',
+    'password': 'syrJBBSPT67At4rs',
+    'host': '34.84.102.182',
+    'database': 'sportdata'
+}
+# 创建数据库连接引擎
+engine = create_engine(f"mysql+mysqlconnector://{db_config['user']}:{db_config['password']}@{db_config['host']}/{db_config['database']}")
+
+for i in range(0, searchDate):
+    beginTime = beginTime + i * 86400000
+    endTime = beginTime + 86400000
+    matchesinfo = one_day_result(beginTime, endTime)
+    df = pd.DataFrame(matchesinfo)
+    # 遍历 DataFrame 的每一行并插入到 MySQL 数据库
+    try:
+        # 尝试将整个 DataFrame 插入到 MySQL 数据库
+        df.to_sql('fb_data', con=engine, if_exists='append', index=False)
+        print(f"第{i+1}日数据已保存")
+    except Exception as e:
+        print(f"第{i+1}日数据批量插入失败，尝试每50行插入")
+        print(e)
+        # 如果批量插入失败，尝试每50行插入
+        for j in range(0, len(df), 50):
+            df_chunk = df.iloc[j:j+50]
+            try:
+                df_chunk.to_sql('fb_data', con=engine, if_exists='append', index=False)
+                print(f"第{i+1}日第{j//50+1}批数据已保存")
+            except Exception as e:
+                print(f"第{i+1}日第{j//50+1}批数据插入失败，尝试逐行插入")
+                print(e)
+                # 如果每50行插入失败，逐行插入
+                for index, row in df_chunk.iterrows():
+                    try:
+                        row.to_frame().T.to_sql('fb_data', con=engine, if_exists='append', index=False)
+                        print(f"第{i+1}日第{index+1}行数据已保存")
+                        print(row)
+                    except Exception as e:
+                        print(f"第{i+1}日第{index+1}行数据保存失败")
+                        print(e)
+
