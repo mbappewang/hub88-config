262ecc6db15a063d02c9b0c45dbdec967bde60a6
mbappewang <wys9404@gmail.com>
2024-12-17 02:16:39 +0800

    新增获取Hub88比赛数据功能；实现对比赛列表的请求及错误处理

--- 生成hub88比赛信息.py
+++ 生成hub88比赛信息.py
@@ -75,7 +75,37 @@ def getHub88League(sportId,type,hours):
   logger.error(f"更新Live数据失败，重试了 {max_retries} 次，仍未成功")
   return None
 
-results = getHub88League(1,1,0)
+def getHub88Match(sportId,type,hours,leagueId):
+    url = "https://bet-hub88.prod.wintokens.io/hub88/league/matches"
+    max_retries = 10
+    timeout = 10
+    params = {
+    "sportId": sportId,
+    "type": type, 
+    "hours": hours,
+    "leagueId": leagueId
+    }
+
+    for attempt in range(max_retries):
+        try:
+            response = requests.get(url, timeout=timeout, params=params)
+            
+            if response.status_code == 200:
+                return response.json()
+
+            logger.error(f"更新LiveMatchList数据失败, 状态码: {response.status_code}")
+        
+        except requests.Timeout:
+            logger.error(f"请求LiveMatchList超时，第 {attempt + 1}/{max_retries} 次重试")
+        
+        except requests.RequestException as e:
+            logger.error(f"请求LiveMatchList发生错误：{e}, 第 {attempt + 1}/{max_retries} 次重试")
+
+    logger.error(f"更新Live数据失败，重试了 {max_retries} 次，仍未成功")
+    return None
+
+results = getHub88Match(1,2,0,29)
+# print(results)
 data  = results.get('data', [])
 for i in data:
   print(i)
